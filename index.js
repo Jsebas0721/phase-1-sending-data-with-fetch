fb8fc0c19b91abab3adba38943ec8e54c6b9f41e
sebastian medina <sebastianlopez_21@hotmail.com>
2022-09-07 10:25:46 -0400

    commit tree 8c85362d3459cff61fa581dcf617db196c8e9307

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email){
+
+
+    const configObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        }),
+    }
+   return fetch(`http://localhost:3000/users`, configObject)
+    .then(res => res.json())
+    .then(data => {
+        document.body.innerHTML = data.id;
+    })
+    .catch(function(error){
+        alert('Unauthorized Access');
+        document.body.innerHTML = error.message;
+    })
+    
+    
+}
